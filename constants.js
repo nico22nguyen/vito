f1a28bdf8a6fac88c4cdc9223172a47bd7cd36b6
Nic <nico.nguyen22@gmail.com>
2022-12-27 16:35:35 -0600

    added constants for better wall/platform detection

--- constants.js
+++ constants.js
@@ -7,12 +7,13 @@ const SKY_COLOR = '#7ec0ee'
 const GROUND_COLOR = '#7cfc00'
 
 // game constants
+const SCREEN_WIDTH = 1350
+const SCREEN_HEIGHT = 600
 const FPS = 40
 const GRAVITY_FORCE = -1
 const OUT_OF_BOUNDS = 5715
-const SCREEN_WIDTH = 1350
-const SCREEN_HEIGHT = 600
 const GROUND_THICKNESS = 200
+const WALL_THICKNESS = 400
 const KEYCODE_TO_DIRECTION = {
   65: 'LEFT', // A
   68: 'RIGHT', // S
@@ -35,3 +36,5 @@ const PLATFORMS_PER_LEVEL = 10
 // vito constants
 const JUMP_POWER = 25
 const X_VELOCITY = 10
+const SHOE_WIDTH = 20 // for platform detection
+const HAT_JUT = 35 // for wall detection (UNSAFE, THIS IS NOT TIED TO THE ACTUAL HAT DRAWING)
