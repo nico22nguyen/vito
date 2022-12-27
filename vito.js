a1fea6b5e02467ccb29b1d567eb2f3b70a5de14a
Nic <nico.nguyen22@gmail.com>
2022-12-27 15:43:04 -0600

    fixed falling through platforms bug

--- vito.js
+++ vito.js
@@ -1,110 +1,48 @@
-// colors
-const OVERALL_COLOR = '#1560bd'
-const OVERALL_BUTTON_COLOR = '#ffd700'
-const SHOE_COLOR = '#663300'
-const PLATFORM_COLOR = '#ffd700'
-const SKY_COLOR = '#7ec0ee'
-const GROUND_COLOR = '#7cfc00'
-
-// game constants
-const FPS = 40
-const GRAVITY_FORCE = -1
-const OUT_OF_BOUNDS = 5010
-const SCREEN_WIDTH = 1350
-const SCREEN_HEIGHT = 600
-const PLATFORM_SPACING = 275
-const PLATFORM_START = 300
-const GROUND_LEVEL = 0
-const GROUND_THICKNESS = 200
-const KEYCODE_TO_DIRECTION = {
-  65: 'LEFT', // A
-  68: 'RIGHT', // S
-  87: 'UP', // W
-  32: 'UP', // SPACE
-}
-
-// vito constants
-const JUMP_POWER = 25
-const X_VELOCITY = 10
-
 // game variables
 const keys = {}
 const platforms = []
 let xVelocityMultiplier = 1
 let yVelocity = 0
 let xPosition = 675
-let yPosition = GROUND_LEVEL
+let yPosition = 0
 let direction = 'RIGHT'
-
-// populate platforms
-for (let i = 0; i < 50; i++) {
-	platforms[i] = {
-		x: Math.floor(400 * Math.random()) + Math.floor(400 * Math.random()),
-		y: i * PLATFORM_SPACING + GROUND_LEVEL + PLATFORM_START,
-	}
-}
-
-console.log(platforms)
-
-const keysPressed = function (event) {
-  const direction = KEYCODE_TO_DIRECTION[event.keyCode]
-  if (!direction) return
-  if (event.repeat) return
-
-	keys[direction] = true
-
-  // if both left and right are pressed, do nothing
-  if (!keys['LEFT'] || !keys['RIGHT']) {
-
-    // handle x direction changes
-    if (direction === 'LEFT') xVelocityMultiplier = -1
-    else if (direction === 'RIGHT') xVelocityMultiplier = 1
-  }
-
-}
-
-const keysReleased = function (event) {
-  const direction = KEYCODE_TO_DIRECTION[event.keyCode]
-  if (!direction) return
-	keys[direction] = false
-
-  // if we released up, it shouldn't affect x velocity
-  if (direction === 'UP') return
-
-  // if the other direction key is held, switch velocity immediately 
-  if (keys['LEFT']) xVelocityMultiplier = -1
-  if (keys['RIGHT']) xVelocityMultiplier = 1
-}
-
-const platformCheck = function () {
-  if (yPosition <= GROUND_LEVEL) return true
-
-  return platforms.some(platform => {
-    if (
-      yPosition <= platform.y &&
-      yPosition >= platform.y - 5 &&
-      xPosition >= 475 - platform.x &&
-      xPosition <= 675 - platform.x
-    ) {
-      return true
-    }
-  })
-}
 
 const adjustY = () => {
   // can't go below the ground
-	if (yPosition < GROUND_LEVEL) {
-		yPosition = GROUND_LEVEL
+	if (yPosition < 0) {
+		yPosition = 0
     yVelocity = 0
     return
 	}
   
-  // only apply gravity if we're above the ground
+  // only apply gravity if vito is above the ground
 	yVelocity += GRAVITY_FORCE
 
-  // If we're touching a platform, stop y velocity or jump if key is pressed
-	if (platformCheck() && yVelocity <= 0) yVelocity = keys['UP'] ? JUMP_POWER : 0
+  // if vito is moving upwards, no platform logic needed
+  if (yVelocity > 0) {
+    yPosition += yVelocity
+    return
+  }
 
+  // If vito is touching a platform, stop y velocity or jump if key is pressed
+  const intersectingPlatform = platformCheck(yPosition)
+  if (intersectingPlatform) {
+    yVelocity = keys['UP'] ? JUMP_POWER : 0
+    yPosition = intersectingPlatform.y
+    return
+  }
+
+  // if applying velocity would skip vito past the next platform, land him on the platform
+  const nextPlatform = getNextPlatform(yPosition)
+  const nextPosition = yPosition + yVelocity
+
+  if (nextPosition <= 0 || (nextPlatform && nextPosition <= nextPlatform.y && xPosition >= 475 - nextPlatform.x && xPosition <= 675 - nextPlatform.x))  {
+    yVelocity = keys['UP'] ? JUMP_POWER : 0
+    yPosition = nextPlatform.y
+    return
+  }
+
+  // otherwise, apply velocity
   yPosition += yVelocity
 }
 
@@ -125,15 +63,19 @@ const tick = function (context) {
 
 	drawBackground(xPosition, yPosition, context)
 	drawVito(SCREEN_WIDTH / 2, 0, xVelocityMultiplier, context)
-  console.log(xPosition)
 }
 
 const run = () => {
   const canvas = document.getElementById('canvas')
+  canvas.width = SCREEN_WIDTH
+  canvas.height = SCREEN_HEIGHT
+
   const context = canvas.getContext('2d')
 
   document.addEventListener('keydown', keysPressed)
   document.addEventListener('keyup', keysReleased)
+  
+  populatePlatforms()
 
   const _tick = () => tick(context)
   const game = setInterval(_tick, 1000 / FPS)
